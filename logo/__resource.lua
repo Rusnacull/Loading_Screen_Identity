8d3e330cf9094ca14436dac2baa54a8bff255e29
Rusnacull Costy Rus <43711421+Rusnacull@users.noreply.github.com>
2019-03-15 19:43:21 +0200

    Add files via upload

---
+++ logo/__resource.lua
@@ -0,0 +1,16 @@
+files {
+    'bower.json',
+    'package.json',
+    'particles.js',
+    'particles.min.js',
+	'index.html',
+	'particles.json',
+	'bg.gif',
+	'style.css',
+	'app.js'
+	
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
